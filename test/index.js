9051053a9d2ecfcc1fef31679199cfcd91dcaa0a
James Dixon <jim.w.dixon@gmail.com>
2016-07-10 00:08:22 -0600

    add test for sending notification message as an object

--- test/index.js
+++ test/index.js
@@ -4,11 +4,29 @@ import test from 'ava';
 
 // Tests
 
-test('sends a notification', async t => {
+test('sends a notification (message as string)', async t => {
 
     const client = new Client(process.env.APP_ID, process.env.REST_API_KEY);
 
     const message = 'Let the playas play!';
+    const options = {
+        isIos: true,
+        included_segments: 'all'
+    };
+
+    const response = await client.sendNotification(message, options);
+
+    t.is(response.statusCode, 200);
+});
+
+test('sends a notification (message as object)', async t => {
+
+    const client = new Client(process.env.APP_ID, process.env.REST_API_KEY);
+
+    const message = {
+        en: 'Let the playas play!'
+    };
+
     const options = {
         isIos: true,
         included_segments: 'all'
