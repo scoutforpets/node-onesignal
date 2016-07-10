7485c5046db78e3655230878695c3a392a535e05
James Dixon <jim.w.dixon@gmail.com>
2016-07-09 23:55:43 -0600

    add initial tests

---
+++ test/index.js
@@ -0,0 +1,34 @@
+import Client from '../lib/client';
+
+import test from 'ava';
+
+// Tests
+
+test('sends a notification', async t => {
+
+    const client = new Client(process.env.APP_ID, process.env.REST_API_KEY);
+
+    const message = 'Let the playas play!';
+    const options = {
+        isIos: true,
+        included_segments: 'all'
+    };
+
+    const response = await client.sendNotification(message, options);
+
+    t.is(response.statusCode, 200);
+});
+
+test('handles API error', async t => {
+
+    const client = new Client(process.env.APP_ID, process.env.REST_API_KEY);
+
+    const message = 'Let the playas play!';
+    const options = {
+        isIos: true
+    };
+
+    const response = client.sendNotification(message, options);
+
+    t.throws(response);
+});
